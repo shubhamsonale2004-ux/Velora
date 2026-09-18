0249c3788528bcac5838886066a1f870ec53d692
Shubham <shubhamsonale2004@gmail.com>
2026-09-18 16:21:06 +0530

    Make React bootstrap failure explicit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Velora could not find the #root element in index.html.');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
